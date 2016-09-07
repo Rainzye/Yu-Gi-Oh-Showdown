528f12adfbb8abb5c324d42438175b31d036fc48
zellman01 <zellman01@gmail.com>
2016-09-07 18:07:22 -0400

    Add var "MAX_CHAR_LIMIT"

--- commands.js
+++ commands.js
@@ -1,2 +1,6 @@
 /*Core commands of this application. These should not be edited at any time. Please get approval to edit these by an administrator, or it 
-will likely be ignored./*
+will likely be ignored. Anyone with push access may edit these to an extent*/
+
+'use strict';
+
+var MAX_CHAR_LIMIT = 250;
